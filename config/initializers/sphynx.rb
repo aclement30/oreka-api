e1ad2ccc06b750390ca75dd6be9441392d72feb4
Alexandre Clément <web@alexandreclement.com>
2017-12-28 00:11:53 -0500

    Update config

--- config/initializers/sphynx.rb
+++ config/initializers/sphynx.rb
@@ -1,9 +1,9 @@
 Sphynx.configure do |config|
   config.dispatch_requests = [['POST', /^\/access_token$/]]
   #config.revocation_requests = [['POST', /^logout$/]]
-  config.secret = 'verysecret'
+  config.secret = ENV['OREKA_SPHYNX_SECRET']
   config.scopes = {
       user: { user_class: User, provider_class: AuthProvider }
   }
-  config.google_client_id = '529258741380-bdvaq5l3bd3o1a60opija08s283dbpp4.apps.googleusercontent.com'
+  config.google_client_id = ENV['OREKA_GOOGLE_CLIENT_ID']
 end
